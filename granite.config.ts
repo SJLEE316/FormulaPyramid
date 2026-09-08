70fcebeae183daaec65d595e4963acd435f259b1
SJLEE316 <lsj8249@naver.com>
2026-09-08 14:24:07 +0900

    feat: 로고 및 폰트 추가

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: "수식피라미드", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#ff6fb3", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "/icon600.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
     host: "172.16.11.227",
