111c895ad0c6c04e92b57ebd3718923ab78fa016
SJLEE316 <lsj8249@naver.com>
2026-09-10 16:01:41 +0900

    fix: 브랜드아이콘 경로 수정

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: "수식피라미드", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#ff6fb3", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "/icon600.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/84887/d3897038-a63f-485d-a0d6-7b73adba26ce.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
     host: "172.16.11.227",
